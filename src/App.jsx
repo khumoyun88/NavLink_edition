d38817acc0c2a808323e712f7ede9a5272978c9a
khumoyun88 <davlatovkhumoyun@gmail.com>
2024-06-29 21:09:30 +1400

    project  page updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div>
       <Router>
         <header>
-          <h1>Sehello</h1>
+          <h1>Selhano</h1>
           <nav>
             <ul>
               <li>
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,43 @@
+import './project.css';
+
+
+
 const Project = () => {
-    return <h1>Project Page</h1>;
+    return (
+      <>
+      <div className='title titleProject'>
+        <h1>Our Project</h1>
+      </div>
+
+      <div className='rooms'>
+        <button>bathroom</button>
+        <button>Bed Room</button>
+        <button>Kitchen</button>
+        <button>Living Room</button>
+      </div>
+
+      <div className="cards">
+        <div className="card">
+          <img src="/bad2.png" alt="" />
+          <h4>Bedroom</h4>
+          <p>decor/architecture</p>
+        </div>
+
+        <div className="card">
+          <img src="/bad1.png" alt="" />
+          <h4>Bedroom</h4>
+          <p>decor/architecture</p>
+        </div>
+      </div>
+
+
+      
+      
+  
+
+
+      </>
+    )
   };
   
   export default Project;
