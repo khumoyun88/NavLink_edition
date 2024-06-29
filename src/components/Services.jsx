0e11c98031447cbf878b971258fb4f4a2b9d24cd
khumoyun88 <davlatovkhumoyun@gmail.com>
2024-06-29 20:09:50 +1400

    services page updated

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,68 @@
+import './sevices.css';
+
 const Services = () => {
-    return <h1>Services Page</h1>;
+    return (
+      <>
+      
+      <div className='title'>
+        <h1>Services</h1>
+      </div>
+
+      <div>
+      <div className='readMore'>
+        <div>
+          <h2>Project Plan</h2>
+          <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
+          <button>read more</button>
+        </div>
+
+        <div>
+          <h2>Project Plan</h2>
+          <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
+          <button>read more</button>
+        </div>
+        
+        <div>
+          <h2>Project Plan</h2>
+          <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
+          <button>read more</button>
+        </div>
+      </div>
+
+      <div className='readMore'>
+        <div>
+          <h2>Project Plan</h2>
+          <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
+          <button>read more</button>
+        </div>
+
+        <div>
+          <h2>Project Plan</h2>
+          <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
+          <button>read more</button>
+        </div>
+        
+        <div>
+          <h2>Project Plan</h2>
+          <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
+          <button>read more</button>
+        </div>
+      </div>
+      </div>
+
+      
+      
+      
+      
+      
+      
+      
+      
+      </>
+
+
+
+    )
   };
   
   export default Services;
