c6eb7fdafb7bf0baf45febbed1ebf1a60c52257e
khumoyun88 <davlatovkhumoyun@gmail.com>
2024-06-29 19:42:00 +1400

    main page updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,22 +3,26 @@ const Home = () => {
   return (
    <>
    <div className='main'>
-    <h1>Lets make your home beautiful together</h1>
-    <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
-    <button></button>
+    <div>
+      <h1>Lets make your home beautiful together</h1>
+      <p>There are many variations of the passages of lorem Ipsum fromavailable, majority.</p>
+      <button>Get Started</button>
+    </div>
    </div>
 
-   <div>
+   <div className='readMore'>
     <div>
       <h2>Project Plan</h2>
       <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
       <button>read more</button>
     </div>
+
     <div>
       <h2>Project Plan</h2>
       <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
       <button>read more</button>
     </div>
+    
     <div>
       <h2>Project Plan</h2>
       <p>There are many variations of the passages of lorem Ipsum from available, majority.</p>
